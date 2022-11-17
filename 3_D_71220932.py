2a029252aaf469a23b4ce5e467113bec7a16a1e2
hawkgave <117901120+hawkgave@users.noreply.github.com>
2022-11-17 16:33:43 +0700

    Add files via upload

---
+++ 3_D_71220932.py
@@ -0,0 +1,10 @@
+inputan=input("Masukkan nama Anda: ")
+inputan=input("Masukkan mata kuliah: ")
+inputan=input("Masukkan grup: ")
+
+
+pisah=inputan.split(" ")
+depan=pisah[0]
+belakang=pisah[1]
+print("haloo!", belakang, ",", depan)
+print(
